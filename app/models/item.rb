dbf3b851d924cb497e15b4507562b89e687f2424
Pablo Kaua <pablokauaah@gmail.com>
2024-11-02 16:37:36 -0300

    Refatora view de prato e bebida

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   def acceptable_image
     errors.add(:photo, "não deve ser vazia") unless photo.attached?
 
-    acceptable_types = ["image/jpeg", "image/png", "image/webp"]
+    acceptable_types = ["image/jpeg", "image/png", "image/webp", "image/jpg"]
     unless acceptable_types.include?(photo.content_type)
       errors.add(:photo, "deve ser JPEG ou PNG")
     end
